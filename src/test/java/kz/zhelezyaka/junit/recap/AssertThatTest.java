ff6a72bcfbd1b8127730d5dcdee9f697a228dfa3
VladimirGlinskikh <support@zhelezyaka.kz>
2020-03-26 21:35:44 +0600

    working with compound value matchers - either, both, anyOf, allOf, and not

--- src/test/java/kz/zhelezyaka/junit/recap/AssertThatTest.java
+++ src/test/java/kz/zhelezyaka/junit/recap/AssertThatTest.java
@@ -16,4 +16,19 @@ public class AssertThatTest {
         assertThat(age, not(equalTo(33)));
         assertThat(age, is(not(33)));
     }
+
+    @Test
+    public void verifyMultipleValues() {
+        double marks = 100.00;
+        assertThat(marks, either(is(100.00)).or(is(90.9)));
+
+        assertThat(marks, both(not(99.99)).and(not(60.00)));
+
+        assertThat(marks, anyOf(is(100.00), is(1.00), is(55.00),
+                is(88.00), is(67.8)));
+
+        assertThat(marks, not(anyOf(is(0.00), is(200.00))));
+
+        assertThat(marks, not(allOf(is(1.00), is(100.00), is(30.00))));
+    }
 }
