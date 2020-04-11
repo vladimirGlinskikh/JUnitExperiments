b9d39c057b28ae2a15d4afbf7f3ff9511e124aa6
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-11 17:44:41 +0600

    created simple Money class to be tested

---
+++ src/main/java/kz/zhelezyaka/junit/money/Money.java
@@ -0,0 +1,29 @@
+package kz.zhelezyaka.junit.money;
+
+public class Money {
+    private final int amount;
+    private final String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Money) {
+            Money money = (Money) obj;
+            return money.getCurrency().equals(getCurrency())
+                    && getAmount() == money.getAmount();
+        }
+        return false;
+    }
+}
