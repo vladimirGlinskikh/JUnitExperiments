06bb2e72847ef405d4fc19d62447bb123f6d049d
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-17 19:25:52 +0600

    added test classes as inner classes

--- src/test/java/kz/zhelezyaka/junit/controller/DefaultControllerTest.java
+++ src/test/java/kz/zhelezyaka/junit/controller/DefaultControllerTest.java
@@ -6,6 +6,26 @@ import org.junit.Test;
 public class DefaultControllerTest {
     private DefaultController controller;
 
+    private class SampleRequest implements Request {
+
+        @Override
+        public String getName() {
+            return "Test";
+        }
+    }
+
+    private class SampleHandler implements RequestHandler {
+
+        @Override
+        public Response process(Request request) throws Exception {
+            return new SampleResponse();
+        }
+    }
+
+    private class SampleResponse implements Response {
+
+    }
+
     @Before
     public void instantiate() throws Exception {
         controller = new DefaultController();
