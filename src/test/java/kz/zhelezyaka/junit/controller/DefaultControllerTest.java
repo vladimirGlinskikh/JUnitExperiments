40e64cd6aa0f136647d2516f03a73f44156603a6
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-18 21:04:46 +0600

    a refactored SampleResponse class

--- src/test/java/kz/zhelezyaka/junit/controller/DefaultControllerTest.java
+++ src/test/java/kz/zhelezyaka/junit/controller/DefaultControllerTest.java
@@ -35,7 +35,23 @@ public class DefaultControllerTest {
     }
 
     private class SampleResponse implements Response {
+        private static final String NAME = "Test";
 
+        public String getName() {
+            return NAME;
+        }
+
+        public boolean equals(Object object) {
+            boolean result = false;
+            if (object instanceof SampleResponse) {
+                result = ((SampleResponse) object).getName().equals(getName());
+            }
+            return result;
+        }
+
+        public int hashCode() {
+            return NAME.hashCode();
+        }
     }
 
     @Test
