910aeeebb698271f206f7ee43516a87032b842b1
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-23 00:25:27 +0600

    refactoring method fibonacci()

--- src/main/java/kz/zhelezyaka/junit/recursion/Factorial.java
+++ src/main/java/kz/zhelezyaka/junit/recursion/Factorial.java
@@ -2,18 +2,12 @@ package kz.zhelezyaka.junit.recursion;
 
 public class Factorial {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println("fibonacci(" + i + ") = " + factorial(i));
         }
     }
 
     private static int factorial(int value) {
-        if (value == 0) {
-            return 1;
-        } else if (value == 1) {
-            return 1;
-        } else {
-            return factorial(value -2) + factorial(value - 1);
-        }
+        return (value < 2) ? 1 : factorial(value - 2) + factorial(value - 1);
     }
 }
