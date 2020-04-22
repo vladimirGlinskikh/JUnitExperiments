4472177323e0466352e33d8ce0b49424cf094875
VladimirGlinskikh <support@zhelezyaka.kz>
2020-04-22 18:13:48 +0600

    primitive example a recursion with number of Fibonacci

---
+++ src/main/java/kz/zhelezyaka/junit/recursion/Factorial.java
@@ -0,0 +1,19 @@
+package kz.zhelezyaka.junit.recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("fibonacci(" + i + ") = " + factorial(i));
+        }
+    }
+
+    private static int factorial(int value) {
+        if (value == 0) {
+            return 0;
+        } else if (value == 1) {
+            return 1;
+        } else {
+            return factorial(value -2) + factorial(value - 1);
+        }
+    }
+}
