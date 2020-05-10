7c9c2a7ffc7e9c5b67cb08d1aa9a34c33d3da9e4
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-11 00:11:56 +0600

    example copy method

--- src/main/java/kz/zhelezyaka/io/copy/App.java
+++ src/main/java/kz/zhelezyaka/io/copy/App.java
@@ -1,18 +1,22 @@
 package kz.zhelezyaka.io.copy;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 
 public class App {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        InputStream src = new URL("http://google.com").openStream();
+        OutputStream dst = new FileOutputStream("/././tmp/google.txt");
+        copy(src, dst);
     }
 
     public static void copy(InputStream src, OutputStream dst) throws IOException {
         while (true) {
             int data = src.read();
-            if (data == -1) {
+            if (data != -1) {
                 dst.write(data);
             } else {
                 return;
