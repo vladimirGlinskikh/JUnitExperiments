3e2961006e23fe9ef367c620bbba8c7ff1120d3e
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-11 01:52:06 +0600

    compared implementations

--- src/main/java/kz/zhelezyaka/io/copy/App.java
+++ src/main/java/kz/zhelezyaka/io/copy/App.java
@@ -8,16 +8,29 @@ import java.net.URL;
 
 public class App {
     public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
         InputStream src = new URL("http://google.com").openStream();
         OutputStream dst = new FileOutputStream("/././tmp/google.txt");
         copy(src, dst);
+        long endTime = System.currentTimeMillis();
+        long sum = endTime - startTime;
+        System.out.println(sum);
     }
 
     public static void copy(InputStream src, OutputStream dst) throws IOException {
+//        while (true) {
+//            int data = src.read();
+//            if (data != -1) {
+//                dst.write(data);
+//            } else {
+//                return;
+//            }
+//        }
+        byte[] buff = new byte[64 * 1024];
         while (true) {
-            int data = src.read();
-            if (data != -1) {
-                dst.write(data);
+            int count = src.read(buff);
+            if (count != -1) {
+                dst.write(buff, 0, count);
             } else {
                 return;
             }
