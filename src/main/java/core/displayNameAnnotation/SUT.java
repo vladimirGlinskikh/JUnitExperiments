2fc36dcbefef0959c715f07640ee40bba9d7a1ae
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-19 15:18:26 +0600

    The @DisplayName annotation

---
+++ src/main/java/core/displayNameAnnotation/SUT.java
@@ -0,0 +1,15 @@
+package core.displayNameAnnotation;
+
+public class SUT {
+    public String hello() {
+        return "Hello!";
+    }
+
+    public String talk() {
+        return "How are you?";
+    }
+
+    public String bye() {
+        return "Bye!";
+    }
+}
