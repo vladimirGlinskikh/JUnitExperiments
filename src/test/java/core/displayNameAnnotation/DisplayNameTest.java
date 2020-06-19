c52e5eadfcf61275707666d58b7ed3ecc9a73ca3
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-19 15:27:41 +0600

    Switch on jupiter.api.Test

--- src/test/java/core/displayNameAnnotation/DisplayNameTest.java
+++ src/test/java/core/displayNameAnnotation/DisplayNameTest.java
@@ -1,7 +1,7 @@
 package core.displayNameAnnotation;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
