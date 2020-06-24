56d138b8342295437d276bf06c0fa55f03dc2164
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-24 06:48:05 +0600

    Check in the same test case in JUnit 5

---
+++ src/test/java/architecture/differences/JUnit5SUTTest.java
@@ -0,0 +1,51 @@
+package architecture.differences;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JUnit5SUTTest {
+    private static ResourceForAllTests resourceForAllTests;
+    private SUT systemUnderTest;
+
+    @BeforeAll
+    static void setUpClass() {
+        resourceForAllTests =
+                new ResourceForAllTests("Our resource for all tests");
+    }
+
+    @AfterAll
+    static void tearDownClass() {
+        resourceForAllTests.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        systemUnderTest = new SUT("Our system under test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        systemUnderTest.close();
+    }
+
+    @Test
+    public void testUsualWork() {
+        boolean canReceiveUsualWork =
+                systemUnderTest.canReceiveUsualWork();
+        assertTrue(canReceiveUsualWork);
+    }
+
+    @Test
+    public void testAdditionalWork() {
+        boolean canReceiveAdditionalWork =
+                systemUnderTest.canReceiveAdditionalWork();
+        assertFalse(canReceiveAdditionalWork);
+    }
+
+    @Test
+    @Disabled
+    public void mySecondTest() {
+        assertEquals(2, 1, "2 is not equal to 1");
+    }
+}
